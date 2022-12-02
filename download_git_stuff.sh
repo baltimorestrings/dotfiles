3d3163d65c7c99b5bffff4b1deeb5362fecd6c33
Ari F <aryeh.frankel@verizonmedia.com>
2022-12-02 17:25:14 -0500

    cleanup

--- download_git_stuff.sh
+++
@@ -1,4 +0,0 @@
-curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -o git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-
--- make_links.sh
+++ make_links.sh
@@ -7,5 +7,14 @@ done
 
 [[ -f ~/zsh ]] || mkdir ~/zsh
 
-## install zsh git-completions
-[[ -f ~/zsh/_git ]] || curl  -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+## install/update zsh git-completions
+echo -e "Installing git-completion.zsh as ~/.zsh/_git"
+curl  -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+
+# install the rest of the fleet
+echo -e "Installing/Updating .git-completion.bash, and .git-prompt.sh in ~"
+curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash --silent
+curl -o ~/.git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh --silent
+curl  -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh --silent
+
+
