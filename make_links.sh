852106b6b79905f58748b2d3dc2926359ccaabaf
baltimorestrings <frankelaryeh@gmail.com>
2022-12-02 17:11:09 -0500

    Update make_links.sh

--- make_links.sh
+++ make_links.sh
@@ -4,3 +4,8 @@ echo -e $file_location
 for file in bashrc,path,exports,aliases,functions,extra,secrets,bash_profile,zshrc; do
     [[ -f $file_location/$file ]] && ln -s $file_location/$file ~/.$file
 done
+
+[[ -f ~/zsh ]] || mkdir ~/zsh
+
+## install zsh git-completions
+curl  -o ~/zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
