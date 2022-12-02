8f7e236ea2d5633466e5433381465bd96fd107e6
Ari F <aryeh.frankel@verizonmedia.com>
2022-12-02 14:05:43 -0500

    imgtest

---
+++ download_git_stuff.sh
@@ -0,0 +1,4 @@
+curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
