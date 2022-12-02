081711246bf11dbc5d78f331f6b5c3e306705751
baltimorestrings <frankelaryeh@gmail.com>
2022-12-02 17:09:02 -0500

    Update make_links.sh

--- make_links.sh
+++ make_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 file_location=$(dirname $0)
 echo -e $file_location	
-for file in bashrc,path,exports,aliases,functions,extra,secrets; do
+for file in bashrc,path,exports,aliases,functions,extra,secrets,bash_profile,zshrc; do
     [[ -f $file_location/$file ]] && ln -s $file_location/$file ~/.$file
 done
